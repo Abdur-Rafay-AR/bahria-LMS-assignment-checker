1ca78e2c8742e9838d3feb7ad13fb6959fece637
Abdur Rafay <abdurrafay432007@gmail.com>
2025-01-31 10:49:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ def get_assignments(enroll, password, progress_callback):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920x1080")
     service = Service(ChromeDriverManager().install()) 
     
     results = []
